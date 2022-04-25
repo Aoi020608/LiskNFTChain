3e84bba638baab0c305b27c5fc0f535943a16e61
Aoi <ht.so.ik0102@gmail.com>
2022-04-25 23:27:23 +0700

    peek function

--- data-structures-stacks-and-queues/src/stack.rs
+++ data-structures-stacks-and-queues/src/stack.rs
@@ -1,9 +1,12 @@
 use std::cell::RefCell;
 use std::rc::Rc;
+// RC - multiple owner
+// RefCell - modified
 
 type Link<T> = Option<Rc<RefCell<Node<T>>>>;
 
-struct Node<T> {
+#[derive(Debug)]
+pub struct Node<T> {
     data: T,
     next: Link<T>,
 }
@@ -61,6 +64,31 @@ impl<T> Stack<T> {
             Rc::try_unwrap(prev_top).ok().unwrap().into_inner().data
         })
     }
+
+    pub fn peek(&mut self) -> Option<RefCell<Node<T>>> {
+        // match self.top.as_ref() {
+        //     None => None,
+        //     Some(node) => {
+        //         let clone_node = Rc::clone(&node);
+        //         Rc::try_unwrap(clone_node).ok().unwrap().into_inner().data
+        //     }
+        // }
+        // self.top.take() RefCell
+        // if let Some(top_node) = self.top.take() {
+        //     Rc::try_unwrap(top_node).ok()
+        // } else {
+        //     None
+        // }
+        // self.top.take() Data; RefCell { value: Node { data: 2, next: None } }
+        match self.top.take() {
+            None => None,
+            Some(ref_cell) => {
+                Some(ref_cell.borrow_mut().)
+            }
+        }
+        // let clone_node = Rc::clone(self.top.take());
+        // Rc::try_unwrap(clone_node).ok()
+    }
 }
 
 #[cfg(test)]
@@ -81,8 +109,21 @@ mod tests {
         stack.push(2);
         let length = stack.len();
         assert_eq!(length, 2);
-        stack.pop();
+        let data = stack.pop().unwrap();
         let length = stack.len();
         assert_eq!(length, 1);
+        assert_eq!(data, 2);
+    }
+
+    #[test]
+    fn peek_test() {
+        let mut stack = Stack::new();
+        stack.push(1);
+        stack.push(2);
+        // let length = stack.len();
+        // assert_eq!(length, 2);
+        let data = stack.peek().expect("Found error");
+        println!("Data; {:?}", data);
+        // assert_eq!(data, 2);
     }
 }

7c8e8225edea37faf0752c4bf2a893b3110dfb1d
Aoi <ht.so.ik0102@gmail.com>
2022-04-27 06:43:17 +0700

    bubble sort

--- algorithms-sorting/src/bubble_sort.rs
+++ algorithms-sorting/src/bubble_sort.rs
@@ -4,9 +4,9 @@ pub fn bubble_sort(arr: &mut [u32]) -> &mut [u32] {
 
     while !sorted {
         sorted = true;
-        for i in 0..length-1 {
-            if arr[i] > arr[i+1] {
-                arr.swap(i, i+1);
+        for i in 0..length - 1 {
+            if arr[i] > arr[i + 1] {
+                arr.swap(i, i + 1);
                 sorted = false
             }
         }
@@ -16,6 +16,21 @@ pub fn bubble_sort(arr: &mut [u32]) -> &mut [u32] {
     arr
 }
 
+pub fn bubble_sort1(arr: &mut [u32]) -> &mut [u32] {
+    let length = arr.len();
+
+    for i in 0..length {
+        for j in 0..length - 1 {
+            if arr[j] > arr[j + 1] {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    arr
+}
 
 #[cfg(test)]
 mod tests {
@@ -25,6 +40,15 @@ mod tests {
     fn test_bubble_sort() {
         let mut v1 = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
         let sorted = bubble_sort(&mut v1);
+        println!("{:?}", sorted);
+        assert_eq!(sorted, [0, 1, 2, 4, 5, 6, 44, 63, 87, 99, 283]);
+    }
+
+    #[test]
+    fn test_bubble_sort1() {
+        let mut v1 = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+        let sorted = bubble_sort1(&mut v1);
+        println!("{:?}", sorted);
         assert_eq!(sorted, [0, 1, 2, 4, 5, 6, 44, 63, 87, 99, 283]);
     }
 }
